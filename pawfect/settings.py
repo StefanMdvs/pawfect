12aac6a29b70d2dc5d7126e1243e28a2625ff9f0
StefanMdvs <stefan.medves@gmail.com>
2022-01-21 21:56:47 +0000

    Remove secret key and set debug

--- pawfect/settings.py
+++ pawfect/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!t)i^&5#1c@^o!8e2v$o8t)2jjighqf0f%^*5d$m1bbbmfh^lf'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['pawfect-sm.herokuapp.com', 'localhost']
 
