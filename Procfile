58e1e67bcb44b524f30ad77ab40e54cdb42cef2f
StefanMdvs <stefan.medves@gmail.com>
2022-01-21 21:47:19 +0000

    Add Procfile and allowed hosts

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn pawfect.wsgi:application
--- pawfect/settings.py
+++ pawfect/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-!t)i^&5#1c@^o!8e2v$o8t)2jjighqf0f%^*5d$m1bbbmfh^lf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pawfect-sm.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -119,12 +119,21 @@ WSGI_APPLICATION = 'pawfect.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
+
 
 
 # Password validation
