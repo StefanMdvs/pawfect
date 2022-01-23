167f886e6a281d597a41899b61d7cde2d0ac9aba
StefanMdvs <stefan.medves@gmail.com>
2022-01-23 01:22:57 +0000

    Add comment class, url, view and template

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-# from . import views
-from .views import PostList, PostDetail
+from . import views
+from .views import PostList
 
 urlpatterns = [
     path('', PostList.as_view(), name='posts'),
-    path('<slug:slug>/', PostDetail.as_view(), name='post_detail'),
+    path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
+from django.contrib import messages
+
 from .models import Post
+from .forms import CommentForm
 
 
 class PostList(ListView):
@@ -9,6 +12,31 @@ class PostList(ListView):
     template_name = 'blog/posts.html'
 
 
-class PostDetail(DetailView):
-    model = Post
+def post_detail(request, slug):
     template_name = 'blog/post_detail.html'
+    post = get_object_or_404(Post, slug=slug)
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    # Comment posted
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+            messages.success(request, 'Thank you, your comment is awaiting approval by Admin')
+            comment_form = CommentForm()
+    else:
+        comment_form = CommentForm()
+
+    context = {
+        'post': post,
+        'comments': comments,
+        'new_comment': new_comment,
+        'comment_form': comment_form,
+    }
+
+    return render(request, template_name, context)
